4cdfaed7b2be6885917ff6a1cd54911d7419a562
Kibooli Felix <kiboolif@gmail.com>
2025-04-26 20:15:34 +0300

    More updates

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(e) {
+    console.log('Service Worker: Installed');
+    e.waitUntil(
+      caches.open('sheet-form-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          'index.html',
+          'manifest.json',
+          'style.css', 
+          'script.js', 
+          'icon-min.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(e) {
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  });
+  
