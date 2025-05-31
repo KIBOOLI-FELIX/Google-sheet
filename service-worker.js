adc811e65dcbe7136610780e1a38b42a1c9df551
Kibooli Felix <kiboolif@gmail.com>
2025-05-31 12:41:22 +0300

    modify service worker

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,47 @@
-self.addEventListener('install', function(e) {
-    console.log('Service Worker: Installed');
-    e.waitUntil(
-      caches.open('sheet-form-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          'index.html',
-          'manifest.json',
-          'style.css', 
-          'icon-min.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(e) {
-    console.log('Service Worker: Fetching');
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
-  });
-  
+const CACHE_NAME = 'sheet-form-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/style.css', // Include only if you have it
+  '/icon-min.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+];
+
+// INSTALL EVENT
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// ACTIVATE EVENT (clean up old caches if any)
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activate');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('[Service Worker] Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// FETCH EVENT
+self.addEventListener('fetch', (event) => {
+  console.log('[Service Worker] Fetching:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Return cache or fetch from network
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
