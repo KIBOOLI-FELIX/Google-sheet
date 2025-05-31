1c9413a75842a7abb0b679edea8154b8f7278d4f
Kibooli Felix <kiboolif@gmail.com>
2025-05-31 12:56:10 +0300

    More updates

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,9 @@
 const CACHE_NAME = 'sheet-form-cache-v1';
 const ASSETS_TO_CACHE = [
-  '/',
-  'index.html',
-  'manifest.json',
-  'style.css', // Include only if you have it
-  'icon-min.png',
+  './index.html',
+  './manifest.json',
+  './style.css', // Include only if you have it
+  './icon-min.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
 ];
 
